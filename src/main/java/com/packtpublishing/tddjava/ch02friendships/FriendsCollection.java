bcf15436f0e3bd3ad88955abab80768a1465be53
nelsgued <nelson.guedes@tele.com>
2019-01-24 17:42:47 +0100

    First TDD project

---
+++ src/main/java/com/packtpublishing/tddjava/ch02friendships/FriendsCollection.java
@@ -0,0 +1,26 @@
+package com.packtpublishing.tddjava.ch02friendships;
+
+import java.net.UnknownHostException;
+
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
+public class FriendsCollection {
+    private MongoCollection friends;
+
+    public FriendsCollection() throws UnknownHostException {
+        DB db = new MongoClient().getDB("friendships");
+		friends = new Jongo(db).getCollection("friends");
+    }
+
+    public Person findByName(String name) {
+        return friends.findOne("{_id: #}", name).as(Person.class);
+    }
+
+    public void save(Person p) {
+        friends.save(p);
+    }
+}
